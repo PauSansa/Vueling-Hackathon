2409d9015c8212d01516027a1e11b58904416e68
Sansa <pausansa59@gmail.com>
2023-02-06 22:29:44 +0100

    Funcionalidad db

--- src/dbManager.java
+++ src/dbManager.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.stream.Collectors;
 
 public class dbManager {
     private Connection conn;
@@ -14,6 +15,26 @@ public class dbManager {
             statement = conn.createStatement();
 
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertTrip(Trip trip){
+        String name = trip.getName();
+        String type = trip.getType();
+        int duration = trip.getDuration();
+        String cities = String.join(", ", trip.getCities());
+        String details = trip.getDetails().entrySet().stream()
+                .map(entry -> entry.getKey() + " " + entry.getValue())
+                .collect(Collectors.joining(", "));
+
+        try {
+            statement.executeUpdate(String.format(
+                    "INSERT INTO prueba_trips (name, type, duration, cities, details)" +
+                            " VALUES('%s', '%s', %d, '%s', '%s')",
+                    name, type, duration, cities, details
+            ));
+        } catch(SQLException e){
             e.printStackTrace();
         }
     }
